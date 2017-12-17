ea5a00c321fdb47daad665bf615f270913a88697
Darius Afchar <darius.afchar@live.fr>
2017-12-17 17:05:15 +0100

    Add Pizza

--- src/pages/Home/Home.component.js
+++ src/pages/Home/Home.component.js
@@ -1,38 +1,44 @@
-// @flow
-
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, TouchableOpacity } from 'react-native';
 
 import { Page } from 'anibamtion/src/components';
 
 import styles from './Home.style';
 
+const Button = props => (
+  <TouchableOpacity style={styles.button}>
+    <Text style={styles.buttonText}>{props.text}</Text>
+  </TouchableOpacity>
+);
+
 export default class Home extends Component {
   static navigationOptions = {
-    title: 'Home',
+    title: 'Résumé de ma commande',
+    headerTitleStyle: { textAlign: 'center', alignSelf: 'center' },
+    headerStyle: {
+      borderBottomWidth: 0,
+      backgroundColor: 'rgb(249,249,249)',
+    },
   };
-  props: PropsType;
 
   render() {
     return (
-      <Page>
+      <Page backgroundColor="rgb(249,249,249)">
         <View style={styles.container}>
-          <Text style={styles.welcome}>
-            Welcome to React Native!
-          </Text>
-          <Text style={styles.instructions}>
-            This is the Home page
-          </Text>
-          <Text style={styles.instructions}>
-            Double tap R on your keyboard to reload,{'\n'}
-            Shake or press menu button for dev menu
-          </Text>
+          <View style={styles.wrapper}>
+            <Image
+              source={require('anibamtion/src/assets/pizza.png')}
+              style={{ width: 300, height: 200 }}
+              resizeMode="contain"
+            />
+            <View style={styles.textContainer}>
+              <Text style={styles.title}>Pizza quatre-fromages</Text>
+              <Text style={styles.subtitle}>Prix : 8€</Text>
+            </View>
+          </View>
+          <Button text="Valider" />
         </View>
       </Page>
     );
   }
 }
-
-type PropsType = {
-  navigation: any,
-};
--- src/pages/Home/Home.style.js
+++ src/pages/Home/Home.style.js
@@ -1,23 +1,46 @@
 // @flow
 
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import theme from 'anibamtion/src/theme';
+
+const mainColor = '#6c8aa7';
+const secondaryColor = '#455370';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
+    paddingVertical: 20,
+  },
+  wrapper: {
     justifyContent: 'center',
     alignItems: 'center',
+    flex: 1,
   },
-  welcome: {
-    ...theme.fonts.header,
-    textAlign: 'center',
-    margin: theme.grid.x1,
+  title: {
+    fontWeight: '600',
+    fontSize: 22,
+    marginBottom: 5,
+    color: mainColor,
   },
-  instructions: {
-    ...theme.fonts.default,
+  subtitle: {
+    fontSize: 18,
+    color: '#777',
+  },
+  price: {
+    color: mainColor,
+    textAlign: 'right',
+  },
+  textContainer: {
+    marginBottom: 40,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: mainColor,
+    borderRadius: 3,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
     textAlign: 'center',
-    color: theme.colors.darkGray,
-    marginBottom: theme.grid.x1,
   },
 });
